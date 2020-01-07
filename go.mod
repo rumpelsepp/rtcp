792d905604491f7fc6cd7b967b18ce61600fb975
Stefan Tatschner <stefan@rumpelsepp.org>
2020-01-07 21:39:50 +0100

    Initial commit

---
+++ go.mod
@@ -0,0 +1,9 @@
+module git.sr.ht/~rumpelsepp/rtcp
+
+go 1.13
+
+require (
+	git.sr.ht/~rumpelsepp/helpers v0.0.0-20191105065203-157bafb88180
+	git.sr.ht/~rumpelsepp/rlog v0.0.0-20191119152513-6f7f3bf18e94
+	git.sr.ht/~sircmpwn/getopt v0.0.0-20190808004552-daaf1274538b
+)
---
+++ main.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"net"
+	"os"
+
+	"git.sr.ht/~rumpelsepp/helpers"
+	"git.sr.ht/~rumpelsepp/rlog"
+	"git.sr.ht/~sircmpwn/getopt"
+)
+
+type runtimeOptions struct {
+	from    string
+	to      string
+	verbose bool
+	help    bool
+}
+
+func handleClient(conn net.Conn, dst string) {
+
+	fromConn := conn
+	toConn, err := net.Dial("tcp", dst)
+	if err != nil {
+		rlog.Warning(err)
+		return
+	}
+
+	rlog.Debugf("established connection to: %s", dst)
+
+	if _, _, err = helpers.BidirectCopy(fromConn, toConn); err != nil {
+		rlog.Warning(err)
+	}
+}
+
+func main() {
+	opts := runtimeOptions{}
+	getopt.StringVar(&opts.from, "f", "", "specify address mapping from")
+	getopt.StringVar(&opts.to, "t", "", "specify address mapping to")
+	getopt.BoolVar(&opts.verbose, "v", false, "enable debugging output")
+	getopt.BoolVar(&opts.help, "h", false, "show this page and exit")
+
+	err := getopt.Parse()
+	if err != nil {
+		rlog.Crit(err)
+	}
+
+	if opts.help {
+		getopt.Usage()
+		os.Exit(0)
+	}
+
+	if opts.verbose {
+		rlog.SetLogLevel(rlog.DEBUG)
+	}
+
+	if opts.from == "" || opts.to == "" {
+		rlog.Crit("no address mapping specified")
+	}
+
+	ln, err := net.Listen("tcp", opts.from)
+	if err != nil {
+		rlog.Crit(err)
+	}
+
+	rlog.Infof("started rumpelsepp's rtcp server")
+	rlog.Infof("serving from '%s' to '%s'", opts.from, opts.to)
+
+	for {
+		conn, err := ln.Accept()
+		if err != nil {
+			rlog.Warning(err)
+			continue
+		}
+
+		rlog.Debugf("got connection from: %v", opts.from)
+
+		go handleClient(conn, opts.to)
+	}
+}
