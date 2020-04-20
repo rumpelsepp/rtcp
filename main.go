cd3740bd9191d8f3924e31d6c0480389baf5cc2c
Stefan Tatschner <stefan@rumpelsepp.org>
2020-04-20 15:45:23 +0200

    Add tcp keepalive options

--- main.go
+++ main.go
@@ -3,39 +3,61 @@ package main
 import (
 	"net"
 	"os"
+	"time"
 
 	"git.sr.ht/~rumpelsepp/helpers"
 	"git.sr.ht/~rumpelsepp/rlog"
 	"git.sr.ht/~sircmpwn/getopt"
 )
 
-func handleClient(conn net.Conn, dst string) {
-	fromConn := conn
+func handleClient(conn *net.TCPConn, dst string, keepAlive bool, keepAliveTime int) {
+	fromTCPConn := conn
 	toConn, err := net.Dial("tcp", dst)
 	if err != nil {
 		rlog.Warning(err)
 		return
 	}
 
+	toTCPConn := toConn.(*net.TCPConn)
+
+	if keepAlive {
+		if err := fromTCPConn.SetKeepAlive(true); err != nil {
+			rlog.Warningf("Set KeepAlive failed: %s", err)
+		}
+		if err := fromTCPConn.SetKeepAlivePeriod(time.Duration(keepAliveTime) * time.Second); err != nil {
+			rlog.Warning("Set KeepAlivePeriod failed: %s", err)
+		}
+		if err := toTCPConn.SetKeepAlive(true); err != nil {
+			rlog.Warningf("Set KeepAlive failed: %s", err)
+		}
+		if err := toTCPConn.SetKeepAlivePeriod(time.Duration(keepAliveTime) * time.Second); err != nil {
+			rlog.Warning("Set KeepAlivePeriod failed: %s", err)
+		}
+	}
+
 	rlog.Debugf("established connection: %s", toConn.RemoteAddr())
 	defer rlog.Debugf("association lost: %s<->%s", conn.RemoteAddr(), toConn.RemoteAddr())
 
-	if _, _, err = helpers.BidirectCopy(fromConn, toConn); err != nil {
+	if _, _, err = helpers.BidirectCopy(fromTCPConn, toTCPConn); err != nil {
 		rlog.Debug(err)
 	}
 }
 
 type runtimeOptions struct {
-	listen  string
-	to      string
-	verbose bool
-	help    bool
+	listen        string
+	keepAlive     bool
+	keepAliveTime int
+	to            string
+	verbose       bool
+	help          bool
 }
 
 func main() {
 	opts := runtimeOptions{}
 	getopt.StringVar(&opts.listen, "l", ":8000", "listen on this addr:port")
 	getopt.StringVar(&opts.to, "t", "", "specify address mapping to")
+	getopt.BoolVar(&opts.keepAlive, "a", false, "enable tcp keepalive probes")
+	getopt.IntVar(&opts.keepAliveTime, "k", 25, "specify keepalive time in seconds")
 	getopt.BoolVar(&opts.verbose, "v", false, "enable debugging output")
 	getopt.BoolVar(&opts.help, "h", false, "show this page and exit")
 
@@ -73,6 +95,6 @@ func main() {
 		}
 
 		rlog.Debugf("got connection: %s", conn.RemoteAddr())
-		go handleClient(conn, opts.to)
+		go handleClient(conn.(*net.TCPConn), opts.to, opts.keepAlive, opts.keepAliveTime)
 	}
 }
